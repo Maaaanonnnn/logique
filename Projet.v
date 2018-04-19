a3da36ce08e99999c7d6ead46ce9eb10bfc1f7dd
Maaaanonnnn <36541793+Maaaanonnnn@users.noreply.github.com>
2018-04-19 15:31:55 +0200

    Add files via upload

---
+++ Projet.v
@@ -0,0 +1,179 @@
+(**Les entiers binaires*)
+
+Inductive bin : Set :=
+|Zero : bin
+|Double : bin -> bin
+|DoubleOne : bin -> bin.
+
+
+
+(** question 1*)
+
+
+(** g équivalent à une fonction bin to nat*)
+
+Fixpoint g (n : bin) := match n with
+  |Zero => 0
+  |Double n => (g n) + (g n)
+  |DoubleOne n => (g n) + (g n)+1
+end.
+
+
+
+
+Print well_founded.
+Print Acc.
+Print lt.
+
+
+
+Fixpoint Succ (x : bin) := match x with
+  |Zero => DoubleOne Zero
+  |Double x => DoubleOne x
+  |DoubleOne x => Double (Succ x)
+end.
+
+
+
+
+Fixpoint f (n : nat) := match n with
+  |O => Zero
+  |S m => Succ (f m)
+end.
+
+
+Eval compute in g (f 5).
+
+(**question 2*)
+Require Import Omega.
+
+
+
+Lemma inter1 : forall (n:bin), g (Succ n) = S ( g n ).
+Proof.
+  intro n.
+  induction n.
+  reflexivity.
+  simpl.
+  omega.
+  simpl.
+  rewrite IHn.
+  omega.
+Qed.
+
+
+
+Lemma composition : forall (n : nat), g(f(n)) = n.
+Proof.
+  intro n.
+  induction n.
+  reflexivity.
+  simpl.
+  rewrite inter1.
+  rewrite IHn.
+  reflexivity.
+Qed.
+
+
+(**question 3*)
+
+Eval compute in f (g (Double(Double Zero))).
+
+
+Fixpoint h (b : bin) := match b with
+  |Zero => Zero
+  |Double Zero => Zero
+  |Double b => match (h b) with 
+                     |Zero => Zero
+                     |_ => Double (h b)
+               end
+  |DoubleOne b => DoubleOne (h b)
+end.
+
+Eval compute in h (Double(DoubleOne(Double(Double Zero)))).
+Eval compute in g(h(Double(DoubleOne(Double(Double Zero))))).
+Eval compute in g(Double(DoubleOne(Double(Double Zero)))).
+Eval compute in f(g(h(Double(DoubleOne(Double(Double Zero)))))).
+
+(*question 4*)
+
+
+Lemma inter2 : forall (b:bin), g (h b) = 0 -> h b = Zero.
+Proof.
+intros.
+induction b.
+reflexivity.
+  - now rewrite 
+Qed.
+
+
+Lemma inter3 :  forall (b:bin), b = Zero -> h (Double b) = Zero.
+Proof.
+  intro b.
+  induction b.
+  reflexivity.
+  easy.
+  simpl.
+  easy.
+Qed.
+
+
+
+
+Lemma fsteq : forall (b : bin), g b = g (h b).
+Proof.
+  induction b.
+  - easy.
+  - now apply inter3.
+
+
+ -simpl.
+  rewrite IHb.
+  destruct (h b).
+  simpl.
+  
+Qed.
+
+
+
+
+
+
+Lemma inter5 : forall (b:bin), b = h b.
+Proof.
+  intros.
+  induction b.
+  reflexivity.
+  rewrite IHb.
+  rewrite inter3.
+
+
+ Qed.
+
+
+
+Lemma inter4 : forall (b:bin), Succ b = h (Succ b).
+Proof.
+  intros.
+  induction b.
+  reflexivity.
+  simpl.
+  
+  .
+Qed.
+
+
+
+
+
+
+
+Lemma inter2 : forall (b:bin), Succ (h b) = h (Succ b).
+Proof.
+  intro b.
+  induction b.
+  reflexivity.
+  destruct ( h (Double b)) eqn:hyp.
+Qed.
+
+
---
+++ projet5.v
@@ -0,0 +1,190 @@
+(*rajouter la logique classique*)
+
+Require Import Bool.
+
+(*question 1*)
+Definition pierce := forall (P Q : Prop), ((P -> Q) -> P) -> P.
+
+Definition elimdoublneg := forall (P: Prop), ~~P -> P.
+
+Definition tiersexclu := forall (P:Prop), ~P \/ P.
+
+Definition implication := forall (P Q : Prop), (P -> Q) -> ~P \/ Q.
+
+Definition demorgan := forall (P Q : Prop), ~(~P/\~Q) -> P\/Q.
+
+Definition doubleneg := forall (P : Prop), (~P -> P) -> P.
+
+
+
+
+
+
+
+(*Loi de Pierce => Double négation *)
+Lemma one : pierce -> doubleneg.
+Proof.
+intros.
+unfold pierce in H.
+unfold doubleneg.
+intro P.
+intro hyp.
+apply H with (Q:=False).
+intro hyp1.
+apply hyp.
+assumption.
+Qed.
+
+
+
+(*Définition classique de l'implcation => le tiers-exclu*)
+Lemma two : implication -> tiersexclu.
+Proof.
+intros.
+unfold implication in H.
+unfold tiersexclu.
+intro P.
+apply H.
+intro hyp.
+assumption.
+Qed.
+
+
+(* Double négation => Elimination de la double négation*)
+Lemma three : doubleneg -> elimdoublneg.
+Proof.
+intros.
+unfold doubleneg in H.
+unfold elimdoublneg.
+intro P.
+intro hyp.
+apply H.
+intro hyp1.
+apply hyp in hyp1.
+easy.
+Qed.
+
+
+
+
+(* Le tiers-exclu => la loi de De Morgan *)
+Lemma four : tiersexclu -> demorgan.
+Proof.
+intros.
+unfold tiersexclu in H.
+unfold demorgan.
+intro P.
+intro Q.
+intro hyp.
+pose proof (H P).
+destruct H0.
+pose proof (H Q).
+destruct H1.
+exfalso.
+apply hyp.
+split.
+assumption.
+assumption.
+right.
+assumption.
+left.
+assumption.
+Qed.
+
+
+
+(* La loi de De Morgan => la définition classique de l'implication*)
+Lemma five1 : demorgan -> implication.
+Proof.
+intros.
+unfold demorgan in H.
+unfold implication.
+intros.
+apply H.
+intro hyp.
+destruct hyp.
+apply H1.
+intro hyp1.
+apply H2.
+apply H0 in hyp1.
+easy.
+Qed.
+
+
+
+(* La loi de Pierce => l'élimination de la double négation*)
+Lemma six : pierce -> elimdoublneg.
+Proof.
+intros A B H.
+now apply A with False.
+Qed.
+
+
+
+
+(* Le tiers-exclu => la loi de Pierce*)
+Lemma seven : tiersexclu -> pierce.
+Proof.
+intros hyp A B H.
+destruct (hyp A).
++ now apply H.
++ easy.
+Qed.
+
+
+
+
+(* L'élimination de la double négationn => le tiers-exclu *)
+Lemma eight : elimdoublneg -> tiersexclu.
+Proof.
+intros.
+unfold elimdoublneg in H.
+unfold tiersexclu.
+intros.
+apply (H (~P \/ P)).
+intro hyp1.
+cut (~~P /\ ~P).
+- intro hyp2.
+ now destruct hyp2.
+- split; intro; apply hyp1.
+ now left.
+ now right.
+Qed.
+
+
+(*question 2*)
+
+
+
+
+
+
+
+(*le principe des tiroirs*)
+
+Require Import List.
+
+(*question 1*)
+
+
+Inductive repeats {X : Type} : list X -> Prop :=
+  |l : forall (x : X) (l1 : list X), (In x l1) -> (repeats (x::l1))
+  |l0 :forall (x : X) (l1 : list X), (repeats l1) -> (repeats (x::l1)).
+
+
+(*question 2*)
+
+
+Definition sharesElements (X : Type) (l0 l1 : list X) := (forall n : X, (In n l0) -> (In n l1)).
+Definition drawers (X : Type) (l0 l1 : list X) := (sharesElements X l0 l1) /\ ((length l1) < (length l0)) -> repeats l0.
+
+(*question 3*)
+
+
+
+
+
+
+
+
+
